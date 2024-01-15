6bf1a675609cafec837806121b3f427c5341f356
Logan <logansailer0633@gamil.com>
2024-01-15 21:36:26 +0900

    added styles to change based on user input

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //setup by adding 16 x 16 divs with class "grid"
 //set width to 960/#number of divs
+
+//resets grid to be selected size
+
+function selectSize() {
+    for (let i = 0; i < size*size; i++) {  
+        let block = document.createElement("div");
+        block.style.width = height+"px";
+        block.style.height = height+"px";
+        block.addEventListener("mouseenter", () =>
+            {block.classList.add("hover")});
+        container.appendChild(block);
+    }
+}
+
+let size = 16;
+let height = 960/size;
+const container = document.querySelector(".container");
+selectSize();
