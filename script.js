8d0361c917c3a7af67d8e47279b3195fcfc9eba6
Logan <logansailer0633@gamil.com>
2024-01-15 23:04:30 +0900

    add js to resize canvas

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-//setup by adding 16 x 16 divs with class "grid"
-//set width to 960/#number of divs
-
-//resets grid to be selected size
-
 function selectSize() {
     for (let i = 0; i < size*size; i++) {  
+        let height = 960/size;
         let block = document.createElement("div");
         block.style.width = height+"px";
         block.style.height = height+"px";
-        block.addEventListener("mouseenter", () =>
-            {block.classList.add("hover")});
+        block.addEventListener("mouseenter", () => {
+            block.classList.add("hover")});
         container.appendChild(block);
     }
 }
 
+function resize() {
+    size = prompt("Please enter canvas size as an integer less than 100");
+    if (size > 100) {
+        resize ();
+    }
+    container.innerHTML = "";
+    selectSize()
+}
+
 let size = 16;
-let height = 960/size;
 const container = document.querySelector(".container");
+const button = document.querySelector("button");
+button.addEventListener("click", resize);
 selectSize();
